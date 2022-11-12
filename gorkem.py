55e695567323416e8edf253cc6de0e920c2a17e2
Cabbar Gorkem Gul <gorkemgtech@gmail.com>
2022-11-12 22:27:11 +0300

    added load & prep function

--- gorkem.py
+++ gorkem.py
@@ -3,6 +3,7 @@
 
 import cv2
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 # Create a function to show an image
 def imshow(image, title='', size=8):
@@ -90,3 +91,26 @@ def image_rotation(image_path, scale):
     plt.title('Rotated Image')
     plt.axis(False)
     plt.show()
+
+# Create a function to import an image and resize it to be able to be used with our model
+def load_and_prep_image(filename, img_shape=224, scale=True):
+  """
+  Reads in an image from filename, turns it into a tensor and reshapes into
+  (224, 224, 3).
+  Parameters
+  ----------
+  filename (str): string filename of target image
+  img_shape (int): size to resize target image to, default 224
+  scale (bool): whether to scale pixel values to range(0, 1), default True
+  """
+  # Read in the image
+  img = tf.io.read_file(filename)
+  # Decode it into a tensor
+  img = tf.image.decode_jpeg(img)
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape])
+  if scale:
+    # Rescale the image (get all values between 0 and 1)
+    return img/255.
+  else:
+    return img
